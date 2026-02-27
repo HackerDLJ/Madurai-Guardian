1523f97aa0612f9598fe3ff7dd66569d8b0260a3
D. LUKE JEBASUNDAR <lukejebasundar@gmail.com>
2026-02-27 21:27:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5713755334-6b67a",
+  "appId": "1:990103381282:web:207367801e7f758f9bf17b",
+  "apiKey": "AIzaSyCH39WrpaRFWOjJzU1-pmszaRJNFI-4Pi4",
+  "authDomain": "studio-5713755334-6b67a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "990103381282"
+};
